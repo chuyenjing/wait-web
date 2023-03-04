83586d2e2ec59e4a65d0ee8c13df93b32aa87d79
chuyenjing <95420096+chuyenjing@users.noreply.github.com>
2023-03-05 02:18:48 +0800

    Add files via upload

---
+++ new.js
@@ -0,0 +1,26 @@
+var i = 0;
+var txt = '抱歉 當機處理中' +'\n'+
+    'sorry! we are solving problem';
+var speed = 300;
+var images = new Array();
+images[0] = "https://stickershop.line-scdn.net/stickershop/v1/product/12012046/LINEStorePC/main.png";
+images[1] = "https://stickershop.line-scdn.net/stickershop/v1/sticker/405711578/android/sticker.png";
+setTimeout("changeImage()", 2000);
+var timeoutId = null;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("sorry").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+function changeImage(x) {
+    document.getElementById("img").src = images[x];
+    timeoutId = setTimeout(function() {
+        changeImage((x + 1) % images.length);
+    }, 2000);
+}
+typeWriter();
+changeImage(0);
