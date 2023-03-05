b00275219bafa6ef650e6240273c5bbd13783aeb
chuyenjing <95420096+chuyenjing@users.noreply.github.com>
2023-03-05 13:00:50 +0800

    Update new.js

--- new.js
+++ new.js
@@ -1,7 +1,7 @@
 var i = 0;
 var txt = '抱歉 當機處理中' +'\n'+
-    'sorry! we are solving problem';
-var speed = 300;
+    'sorry! we are solving problem...';
+var speed = 200;
 var images = new Array();
 images[0] = "https://stickershop.line-scdn.net/stickershop/v1/product/12012046/LINEStorePC/main.png";
 images[1] = "https://stickershop.line-scdn.net/stickershop/v1/sticker/405711578/android/sticker.png";
@@ -22,5 +22,18 @@ function changeImage(x) {
         changeImage((x + 1) % images.length);
     }, 2000);
 }
+function start(){
+    var timer = document.querySelector("#timer");
+    var number = 20;
+    setInterval(function(){
+        number -- ;
+        if(number <= 0 ){
+            number = 0;
+            location.href='https://www.youtube.com/';
+        }
+        timer.innerText = number + 0 }, 1000);
+
+}
 typeWriter();
 changeImage(0);
+start();
